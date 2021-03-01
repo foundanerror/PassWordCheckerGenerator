9c7aedeec7a789923684cd7dd9abacd29c6288b4
SlayerThaDev <75696110+SlayerThaDev@users.noreply.github.com>
2021-02-28 22:34:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PassWordCheckerGenerator
+ 
---
+++ main.py
@@ -0,0 +1,24 @@
+
+import random
+Mode = str(input('Chose Mode Please (Password Generator, Password Checker): '))
+tab = ['a','b','c','d','e',"f","g","h","i","j","k","l","m","n","o",'p','[','#','%','$','1','2','A','B','C','D','E']
+
+def mixTable(ok):
+    newTab = []
+    for i in range(len(ok)+1):
+        newTab.append(ok[random.randint(0,len(ok)-1)])
+    return newTab
+
+
+def gen():
+    mixed = mixTable(tab)
+    for i in mixed:
+        print(i)
+    if Mode == "g":
+        new = ''
+        for i in range(0, int(random.randint(9,16))):
+            new += str(mixed[random.randint(1,len(mixed)-1)])
+        return new
+
+print(gen())
+
